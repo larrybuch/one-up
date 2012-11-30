e211855f2c1bafdf64cafd9e82d4b0eb8e38ea52
Larry Buchanan <larrybuch@gmail.com>
2012-11-30 10:48:02 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OneUp::Application.config.secret_token = '4c2e8e09df67c44fc34ba9f38f285e823ad0476b0edd458593f54b2e0c9fe72ff22cfcdd4a57eaad6ec703212d609fdfea9c0eb34021df903b5459157e52a9d0'
