b7380d0ceecb5f161e2b289ba108c481c32bc467
Adrian <adrianbautista8@gmail.com>
2012-11-30 13:38:11 -0500

    edited homecontroller edit and added stripe button, make sure to seed first (with the numericality disabled)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,10 +9,29 @@ class HomeController < ApplicationController
     name = params[:name]
     money = params[:money].to_i
     declaration = params[:declaration]
-    if @money > up.money
-      newup = Up.create(:name => name, :money => money, :declaration => declaration)
+    if money > up.money
+
+      amount = money * 100
+      # times 100 because in cents
+      token = params[:token]
+
+      begin
+        newup = Up.create(:name => name, :money => money, :declaration => declaration)
+        #binding.pry
+        @stripe = Stripe::Charge.create(
+          :amount => amount,
+          :card => token,
+          :description => 'One Up Customer',
+          :currency => 'usd'
+          )
+      rescue => e
+        @error = e.message
+      end
+
     else
       render root_path
     end
+    #binding.pry
+    redirect_to root_path
   end
 end
