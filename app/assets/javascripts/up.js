25508a6c4058111aed2868193eabbad8529b9eb2
Adrian <adrianbautista8@gmail.com>
2012-12-19 16:32:10 -0500

    pageslide

--- app/assets/javascripts/up.js
+++ app/assets/javascripts/up.js
@@ -1,7 +1,5 @@
 $(function() {
-  $('#entry').hide();
   $('.stripe-button').bind('token', token_generated);
-  $('div#show-form').click(toggleForm);
   $('#fittext').fitText(0.6);
   $('#fittext2').fitText(1.5);
 });
@@ -9,8 +7,4 @@ $(function() {
 function token_generated (e, token) {
   $('#show-form input[type="hidden"]').val(token.id);
   $('#show-form').submit();
-}
-
-function toggleForm () {
-  $('#entry').slideToggle();
 }
